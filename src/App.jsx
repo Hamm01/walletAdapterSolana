288c313c455dd7fefd35c28df057ca86848ee470
Himanish kochhar <himanish.kochhar@gmail.com>
2024-07-22 04:00:29 +0530

    Added the wallet home function and home functionlity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React, { FC, useMemo } from 'react'
+import WalletHome from './walletHome'
+import {
+  WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui'
 
 function App() {
-  return <div>Himanish</div>
+  return (
+    <WalletHome>
+      <div>
+        <h1>Wallet adapter functions</h1>
+      </div>
+    </WalletHome>
+  )
 }
 
 export default App
---
+++ src/walletHome.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+  ConnectionProvider,
+  WalletProvider
+} from '@solana/wallet-adapter-react'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+
+import '@solana/wallet-adapter-react-ui/styles.css'
+
+export default function WalletHome({ children }) {
+  const endpoint = 'https://api.devnet.solana.com'
+  return (
+    <div>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>{children}</WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  )
+}
