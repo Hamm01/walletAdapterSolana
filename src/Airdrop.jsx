2229761c6d8c3a42f6f297ca5929b83ea643d81c
Himanish kochhar <himanish.kochhar@gmail.com>
2024-07-27 05:41:44 +0530

    Added the airdrop and footer component

--- src/Airdrop.jsx
+++ src/Airdrop.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export const Airdrop = () => {
   return (
-    <div className="flex min-w-2xl border border-neutral-500 rounded-sm p-4">
-      <input type="text" placeholder="Request Airdrop" />
+    <div className="flex flex-col md:w-[600px] lg:w-[800px] p-4">
+      <h2 className="font-semibold text-2xl text-center">
+        {' '}
+        Solana Airdrop Faucet
+      </h2>
+      <div className="p-6  flex flex-row space-x-2">
+        <input type="number" className="input_Amount" placeholder="Amount" />
+        <button className="cursor-pointer whitespace-nowrap inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 w-32">
+          Confirm Airdrop
+        </button>
+      </div>
     </div>
   )
 }
---
+++ src/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <section className="max-w-7xl mx-auto  p-4 border-t ">
+      <div className="flex justify-between">
+        <p className="text-primary text-xl tracking-tight">
+          Developed By &nbsp;
+          <a
+            href="https://github.com/hamm01"
+            className="emotional_Rescue text-md"
+          >
+            Himanish{' '}
+          </a>
+        </p>
+      </div>
+    </section>
+  )
+}
