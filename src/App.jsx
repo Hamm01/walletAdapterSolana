87c3d41cd57c2b2260781ac222e0a6cb56ae65b4
Himanish kochhar <himanish.kochhar@gmail.com>
2024-07-28 10:21:33 +0530

    changes on basis for added the radix ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import Navbar from './Navbar'
 import WalletContextProvider from './walletProvider'
 import { Airdrop } from './Airdrop'
 import { Footer } from './footer'
+import BalanceSol from './BalanceSol'
 
 function App() {
   return (
-    <div className="bg_color_body text-white w-screen h-screen ">
+    <div className="bg_color_body text-white w-screen h-full ">
       <WalletContextProvider>
         <main className="max-w-7xl mx-auto flex flex-col gap-4 p-4 min-h-[92vh] ">
           <Navbar />
-          <div className="flex flex-col gap-4 items-center justify-center min-h-[300px] border rounded-md">
-            <Airdrop />
+          <div className="flex flex-row gap-2">
+            <div className="grow">
+              <Airdrop />
+            </div>
+            <BalanceSol />
           </div>
+
           <div className="pointer-events-none fixed top-1/4 mb-20 ml-32 right-1/4 -translate-x-1/2 translate-y-1/2 w-52 h-28 bg-fuchsia-500/80 blur-[120px]"></div>
+          <div className="flex flex-col gap-4 "></div>
         </main>
         <Footer />
       </WalletContextProvider>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Theme } from '@radix-ui/themes'
 import App from './App.jsx'
 import './index.css'
+import '@radix-ui/themes/styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </StrictMode>
 )
