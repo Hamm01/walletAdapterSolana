c541aecaa535087961c1601d4189d3b49ed58ee5
Himanish kochhar <himanish.kochhar@gmail.com>
2024-08-15 13:44:33 +0530

    Changes in navbar some ui

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,19 +5,18 @@ import {
 } from '@solana/wallet-adapter-react-ui'
 export default function Navbar() {
   return (
-    <nav className="flex flex-row py-4 justify-between items-center">
+    <nav className="flex flex-row py-4 max-sm:flex-col max-sm:justify-start max-sm:items-start max-sm:gap-4 justify-between items-center">
       <div className="flex items-start gap-2">
         <img
           src="solanaLogo.svg"
           alt="Solana wallet"
-          width="250px"
-          height="250px"
+          className={`mobile:w-[200px] md:w-[250px]`}
         />
-        <h3 className="scroll-m-20 text-md font-semibold tracking-tight">
+        <h3 className="mobile:text-[12px] text-sm md:scroll-m-20 md:text-md font-semibold tracking-tight">
           AirDrop
         </h3>
       </div>
-      <div className="flex justify-center items-center gap-4">
+      <div className="flex mobile:w-full mobile:justify-end justify-center items-center gap-4">
         <WalletMultiButton />
       </div>
     </nav>
